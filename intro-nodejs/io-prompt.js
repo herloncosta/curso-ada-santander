285a52bc3d5efe02fc351d00fe6b1b21a3905a82
herloncosta <herlon36@gmail.com>
2024-12-01 13:56:21 -0300

    chore: remove duplicated code

--- intro-nodejs/io-prompt.js
+++ intro-nodejs/io-prompt.js
@@ -5,31 +5,7 @@ const prompt = rl.createInterface({
     output: process.stdout,
 })
 
-// prompt.question('What is your favorite number?: ', (answer) => {
-//     console.log('Your favorite number multiplied by 2 is: ' + parseInt(answer) * 2)
-//     prompt.close()
-// })
-
-const promptPromise = {
-    question: (question) =>
-        new Promise((resolve, reject) => {
-            try {
-                prompt.question(question, (answer) => resolve(answer))
-            } catch (error) {
-                reject(error)
-            }
-        }),
-    close: () => prompt.close(),
-}
-
-async function main() {
-    const number = await promptPromise.question('What is your favorite number?: ')
-    console.log('Your favorite number multiplied by 2 is: ' + parseInt(number) * 2)
-
-    const color = await promptPromise.question('What is your favorite color?: ')
-    console.log('Your favorite color is: ' + color)
-
-    promptPromise.close()
-}
-
-main()
+prompt.question('What is your favorite number?: ', answer => {
+    console.log('Your favorite number multiplied by 2 is: ' + parseInt(answer) * 2)
+    prompt.close()
+})
